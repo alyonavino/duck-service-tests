92981b1a8092a9377f6684e3df9d7eea436a62ae
Alena <sol.vinogradova2017@yandex.ru>
2025-01-26 02:25:37 +0300

    (AT-2) Clients: refactor old methods and add new

--- src/test/java/clients/DuckActionClient.java
+++ src/test/java/clients/DuckActionClient.java
@@ -23,75 +23,95 @@ public class DuckActionClient extends TestNGCitrusSpringSupport {
     @Autowired
     protected HttpClient duckService;
 
-//    создание уточки
-public void createDuck(TestCaseRunner runner, String color, double height, String material, String sound, String wingsState){
-    runner.$(http().client(duckService)
-            .send()
-            .post("/api/duck/create")
-            .message()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body("{\n" +
-                    "\"color\": \"" + color + "\",\n" +
-                    "\"height\": " + height + ",\n" +
-                    "\"material\": \"" + material + "\",\n" +
-                    "\"sound\": \"" + sound + "\",\n" +
-                    "\"wingsState\": \"" + wingsState + "\"\n" + "}"));
-}
+    //    создание уточки
+    public void createDuck(TestCaseRunner runner, String color, double height, String material, String sound, String wingsState) {
+        runner.$(http().client(duckService)
+                .send()
+                .post("/api/duck/create")
+                .message()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body("{\n" +
+                        "\"color\": \"" + color + "\",\n" +
+                        "\"height\": " + height + ",\n" +
+                        "\"material\": \"" + material + "\",\n" +
+                        "\"sound\": \"" + sound + "\",\n" +
+                        "\"wingsState\": \"" + wingsState + "\"\n" + "}"));
+    }
 
-// удаление уточки
-public void duckDelete(TestCaseRunner runner, String id) {
-    runner.$(http().client(duckService)
-            .send()
-            .delete("/api/duck/delete")
-            .queryParam("id", id));
-}
+    // удаление уточки
+    public void duckDelete(TestCaseRunner runner, String id) {
+        runner.$(http().client(duckService)
+                .send()
+                .delete("/api/duck/delete")
+                .queryParam("id", id));
+    }
 
-// обновление свойств уточки
-public void duckUpdate(TestCaseRunner runner, String color, double height, String id, String material, String sound, String wingsState) {
-    runner.$(http().client(duckService)
-            .send()
-            .put("/api/duck/update")
-            .queryParam("color", color)
-            .queryParam("height", String.valueOf(height))
-            .queryParam("id", id)
-            .queryParam("material", material)
-            .queryParam("sound", sound)
-            .queryParam("wingsState", wingsState));
-}
+    // обновление свойств уточки
+    public void duckUpdate(TestCaseRunner runner, String color, double height, String id, String material, String sound, String wingsState) {
+        runner.$(http().client(duckService)
+                .send()
+                .put("/api/duck/update")
+                .queryParam("color", color)
+                .queryParam("height", String.valueOf(height))
+                .queryParam("id", id)
+                .queryParam("material", material)
+                .queryParam("sound", sound)
+                .queryParam("wingsState", wingsState));
+    }
 
-// заставить уточку полетать
-public void duckFly(TestCaseRunner runner, String id) {
-    runner.$(http().client(duckService)
-            .send()
-            .get("/api/duck/action/fly")
-            .queryParam("id", id));
-}
+    // заставить уточку полетать
+    public void duckFly(TestCaseRunner runner, String id) {
+        runner.$(http().client(duckService)
+                .send()
+                .get("/api/duck/action/fly")
+                .queryParam("id", id));
+    }
 
-// заставить уточку крякать
-public void duckQuack(TestCaseRunner runner, String id, int repetitionCount, int soundCount) {
-    runner.$(http().client(duckService)
-            .send()
-            .get("/api/duck/action/quack")
-            .queryParam("id", id)
-            .queryParam("repetitionCount", String.valueOf(repetitionCount))
-            .queryParam("soundCount", String.valueOf(soundCount)));
-}
+    // заставить уточку крякать
+    public void duckQuack(TestCaseRunner runner, String id, int repetitionCount, int soundCount) {
+        runner.$(http().client(duckService)
+                .send()
+                .get("/api/duck/action/quack")
+                .queryParam("id", id)
+                .queryParam("repetitionCount", String.valueOf(repetitionCount))
+                .queryParam("soundCount", String.valueOf(soundCount)));
+    }
 
-// заставить уточку плавать
-public void duckSwim(TestCaseRunner runner, String id) {
-    runner.$(http().client(duckService)
-            .send()
-            .get("/api/duck/action/swim")
-            .queryParam("id", id));
-}
+    // заставить уточку плавать
+    public void duckSwim(TestCaseRunner runner, String id) {
+        runner.$(http().client(duckService)
+                .send()
+                .get("/api/duck/action/swim")
+                .queryParam("id", id));
+    }
 
-// получение свойств уточки
-public void duckProperties(TestCaseRunner runner, String id) {
-    runner.$(http().client(duckService)
-            .send()
-            .get("/api/duck/action/properties")
-            .queryParam("id", id));
-}
+    // получение свойств уточки
+    public void duckProperties(TestCaseRunner runner, String id) {
+        runner.$(http().client(duckService)
+                .send()
+                .get("/api/duck/action/properties")
+                .queryParam("id", id));
+    }
+
+    // цикл для чётного id
+    public AtomicInteger createDuckWithEvenId(TestCaseRunner runner) {
+        AtomicInteger id;
+        do {
+            createDuck(runner, "yellow", 0.07, "wood", "quack", "ACTIVE");
+            id = extractId(runner);
+        } while (id.get() % 2 != 0);
+        return id;
+    }
+
+// цикл для нечётного id
+    public AtomicInteger createDuckWithOddId(TestCaseRunner runner) {
+        AtomicInteger id;
+        do {
+            createDuck(runner, "yellow", 0.07, "wood", "quack", "ACTIVE");
+            id = extractId(runner);
+        } while (id.get() % 2 == 0);
+        return id;
+    }
 
 // валидирование ответа для всех методов
 public void validateResponse(TestCaseRunner runner, HttpStatus httpStatus, String responseMessage) {
@@ -117,8 +137,8 @@ public void validateProperties(TestCaseRunner runner, String color, String heigh
                 .validate(JsonPathSupport.jsonPath().expression("$.wingsState", wingsState)));
 }
 
-// проверка на правильное создание уточки с материалом
-public void validateWithMaterial(TestCaseRunner runner, String color, String height, String material, String sound, String wingsState) {
+// проверка на правильное создание уточки со всеми свойствами
+public void validateAllProperties(TestCaseRunner runner, String color, String height, String material, String sound, String wingsState) {
     runner.$(http().client(duckService)
             .receive()
             .response(HttpStatus.OK)
